de395dd4e6893f0721f2512bfe0065d0fa2fc6c8
Steve <stevebyerly62@gmail.com>
2020-10-17 13:53:57 -0400

    updated hyperparameter defaults

--- NeuronalTransformerLayers/NeuronBankConfig.py
+++ NeuronalTransformerLayers/NeuronBankConfig.py
@@ -1,5 +1,5 @@
 class NeuronBankConfig():
-    def __init__(self, num_neurons=100, values_len=10, num_heads=10, query_len=11, layer_norm_eps=1e-12, num_duplicates=5, num_iterations=12, use_connectivity=True, connectivity_coefficient=10, expanded_size=3000):
+    def __init__(self, num_neurons=100, values_len=10, num_heads=10, query_len=11, layer_norm_eps=1e-12, num_duplicates=5, num_iterations=7, use_connectivity=True, connectivity_coefficient=10, expanded_size=3000):
         self.num_neurons = num_neurons
         self.values_len = values_len
         self.query_len = query_len
